5e0752e5fa0df5a3a6d4e782b7824109eb4313de
Flavio <flacassa@gmail.com>
2020-09-25 19:30:27 -0300

    Criada a classe Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,13 @@
 class Pessoa:
-    pass
+    # Metodo nada mais é do que uma função, que pertence a uma classe
+    #e portanto sempre está conectada/atrelada a um objeto. Portanto sempre
+    # deve ser declarada em um primeiro parametro o objeto a ser recebido.
+    def cumprimentar(self):
+        return f'olá {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+
