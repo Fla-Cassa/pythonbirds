ed8769946c8befa48e902861551a12cd2337aa1b
Flavio <flacassa@gmail.com>
2020-09-26 12:01:03 -0300

    Criados atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,8 @@
-class Pessoa:
     # Metodo nada mais é do que uma função, que pertence a uma classe
     # e portanto sempre está conectada/atrelada a um objeto. Portanto sempre
     # deve ser declarada em um primeiro parametro o objeto a ser recebido.
+class Pessoa:
+    olhos = 2
     def __init__(self, * filhos, nome=None, idade=25):
         self.idade = idade
         self.nome = nome
@@ -23,7 +24,14 @@ if __name__ == '__main__':
         print((filho.nome))
     giovanna.sobrenome = 'Cassa'
     del giovanna.filhos
+    giovanna.olhos = 1
+    del giovanna.olhos
     print(giovanna.__dict__)
     print((flavio.__dict__))
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(giovanna.olhos)
+    print(flavio.olhos)
+    print(id(Pessoa.olhos), id(giovanna.olhos), id(flavio.olhos))
 
 
