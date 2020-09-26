eb74017a72710a55bdc73b5f65386889d9310377
Flavio <flacassa@gmail.com>
2020-09-25 22:50:34 -0300

    Criados atributo composto filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -2,20 +2,22 @@ class Pessoa:
     # Metodo nada mais é do que uma função, que pertence a uma classe
     # e portanto sempre está conectada/atrelada a um objeto. Portanto sempre
     # deve ser declarada em um primeiro parametro o objeto a ser recebido.
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, * filhos, nome=None, idade=25):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Giovanna')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Flávio'
-    print(p.nome)
-    print(p.idade)
+    giovanna = Pessoa(nome='giovanna')
+    flavio = Pessoa(nome='flavio')
+    print(Pessoa.cumprimentar(flavio))
+    print(id(giovanna))
+    print(giovanna.cumprimentar())
+    print(giovanna.nome)
+    print(giovanna.idade)
+    for filho in giovanna.filhos:
+        print((giovanna.filhos))
